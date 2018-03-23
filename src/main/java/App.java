bc41ea1caad04a59d33bdd7024d25b34d8bbccf4
venkatesh <venkateshbaratambabu@gmail.com>
2018-03-23 11:03:38 +0530

    Intial commit

---
+++ src/main/java/App.java
@@ -0,0 +1,9 @@
+
+public class App {
+	
+	public String sample() {
+		
+		return "Sample program";
+	}
+
+}
---
+++ src/test/java/AppTest.java
@@ -0,0 +1,12 @@
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class AppTest {
+
+	@Test
+	public void test() {
+		
+	}
+
+}
